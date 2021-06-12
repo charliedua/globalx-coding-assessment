a65dbceb8c75eaa7d065b863da14f72cd2b20fd0
Anmol Dua <duaanmol2@gmail.com>
2021-06-12 20:56:15 +1000

    Initial project setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace globalx_coding_assessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
